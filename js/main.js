789b36f2d34ffdd2edd340e921953777fc086759
Pericles Oliveira <pzoliveira@outlook.com>
2021-06-05 16:32:22 -0300

    Aula_01

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var nome = "Péricles Zapata";
+var n1 = 49;
+var n2 = 34;
+var frase = "Canadá é o melhor país do mundo";
+alert(nome + " tem " + n1 + " anos.");
+console.log(n1 - n2);
+console.log(frase.replace("Canadá", "Austrália"));
+console.log(frase.toLowerCase());
+console.log(frase.toUpperCase());
