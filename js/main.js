084eb44881b595b9bcd07727c456fd1b6fa01d31
Pericles Oliveira <pzoliveira@outlook.com>
2021-06-05 16:46:14 -0300

    Aula_02

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
-var nome = "Péricles Zapata";
+//Aula_02
+var frutas = ["laranja", "maçã", "pera"];
+frutas.push("uva");
+console.log(frutas);
+console.log(frutas[0]);
+console.log(frutas.pop());
+console.log(frutas.toString());
+console.log(frutas.join(" - "));
+console.log(frutas.reverse());
+
+var frutas2 = [{nome: "maçã", cor:"vermelha"}, {nome:"pera", cor:"verde"}];
+console.log(frutas2);
+console.log(frutas2[1].cor);
+
+
+//Aula_01
+/*var nome = "Péricles Zapata";
 var n1 = 49;
 var n2 = 34;
 var frase = "Canadá é o melhor país do mundo";
@@ -6,4 +22,4 @@ alert(nome + " tem " + n1 + " anos.");
 console.log(n1 - n2);
 console.log(frase.replace("Canadá", "Austrália"));
 console.log(frase.toLowerCase());
-console.log(frase.toUpperCase());
+console.log(frase.toUpperCase());*/
